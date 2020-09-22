e0d83625fa6016fb455591cc02e3f7039b7755f0
Haris Andreolas <divinedlink90@yahoo.gr>
2020-09-22 18:31:04 +0300

    add dice properties

--- app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/DiceDomain.java
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/DiceDomain.java
@@ -1,12 +1,41 @@
 package aeropresscipe.divinelink.aeropress.generaterecipe;
 
-public class DicesDomain {
+public class DiceDomain {
 
     private int diceID;
-    private String diceText;
+    private int diceTemperature;
+    private String GroundSize;
+    private int brewTime;
+    private String brewingMethod;
+    private int bloomTime;
+    private int bloomWater;
 
-    public DicesDomain(int diceID, String diceText) {
+    private int coffeeAmount;
+    private int brewWaterAmount;
+
+
+
+
+    public DiceDomain(int diceID, String groundSize, int brewTime) {
         this.diceID = diceID;
-        this.diceText = diceText;
+        this.GroundSize = groundSize;
+        this.brewTime = brewTime;
+    }
+
+    public DiceDomain(int diceID, int coffeeAmount, int brewWaterAmount) {
+        this.diceID = diceID;
+        this.coffeeAmount = coffeeAmount;
+        this.brewWaterAmount = brewWaterAmount;
+    }
+
+    public DiceDomain(int diceID, int diceTemperature) {
+        this.diceID = diceID;
+        this.diceTemperature = diceTemperature;
+    }
+
+    public DiceDomain(int diceID, String brewingMethod, int bloomTime, int bloomWater) {
+        this.diceID = diceID;
+        this.brewingMethod = brewingMethod;
+        this.bloomWater = bloomWater;
     }
 }
--- app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/GenerateRecipeInteractorImpl.java
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/GenerateRecipeInteractorImpl.java
@@ -1,4 +1,96 @@
 package aeropresscipe.divinelink.aeropress.generaterecipe;
 
-class GenerateRecipeInteractorImpl {
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+public class GenerateRecipeInteractorImpl implements GenerateRecipeInteractor{
+
+            @Override
+            public void getRecipe(OnGenerateRecipeFinishListener listener) {
+
+                listener.onSuccess();
+            }
+
+            private ArrayList<DiceDomain> addTemperatureDiceProperties() {
+
+                ArrayList<DiceDomain> TemperatureDice = new ArrayList<>();
+
+                DiceDomain tempDice1 = new DiceDomain(1, 80);
+                DiceDomain tempDice2 = new DiceDomain(1, 85);
+                DiceDomain tempDice3 = new DiceDomain(1, 90);
+                DiceDomain tempDice4 = new DiceDomain(1, 95);
+                DiceDomain tempDice5 = new DiceDomain(1, 100);
+
+                TemperatureDice.add(tempDice1);
+                TemperatureDice.add(tempDice2);
+                TemperatureDice.add(tempDice3);
+                TemperatureDice.add(tempDice4);
+                TemperatureDice.add(tempDice5);
+
+                return TemperatureDice;
+            }
+
+            private ArrayList<DiceDomain> addGroundSizeDiceProperties(){
+
+                ArrayList<DiceDomain> GroundSizeDice = new ArrayList<>();
+                DiceDomain groundSizeDice1 = new DiceDomain(2,"Fine", 60);
+                DiceDomain groundSizeDice2 = new DiceDomain(2,"MediumFine", 90);
+                DiceDomain groundSizeDice3 = new DiceDomain(2,"Medium", 120);
+                DiceDomain groundSizeDice4 = new DiceDomain(2,"Coarse", 240);
+
+                GroundSizeDice.add(groundSizeDice1);
+                GroundSizeDice.add(groundSizeDice2);
+                GroundSizeDice.add(groundSizeDice3);
+                GroundSizeDice.add(groundSizeDice4);
+
+                return GroundSizeDice;
+            }
+
+            private ArrayList<DiceDomain> addBrewingMethodProperties(){
+
+                ArrayList<DiceDomain> BrewingMethodDice = new ArrayList<>();
+
+                DiceDomain brewingMethodDice1 = new DiceDomain(3, "Standard", 0, 0);
+                DiceDomain brewingMethodDice2 = new DiceDomain(3, "Standard", 30, 30);
+                DiceDomain brewingMethodDice3 = new DiceDomain(3, "Standard", 30, 60);
+                DiceDomain brewingMethodDice4 = new DiceDomain(3, "Inverted", 0, 0);
+                DiceDomain brewingMethodDice5 = new DiceDomain(3, "Inverted", 30, 30);
+                DiceDomain brewingMethodDice6 = new DiceDomain(3, "Inverted", 30, 60);
+
+                BrewingMethodDice.add(brewingMethodDice1);
+                BrewingMethodDice.add(brewingMethodDice2);
+                BrewingMethodDice.add(brewingMethodDice3);
+                BrewingMethodDice.add(brewingMethodDice4);
+                BrewingMethodDice.add(brewingMethodDice5);
+                BrewingMethodDice.add(brewingMethodDice6);
+
+                return BrewingMethodDice;
+            }
+
+            private ArrayList<DiceDomain> addBrewingWaterAmountProperties() {
+                ArrayList<DiceDomain> BrewingWaterAmountDice = new ArrayList<>();
+
+                DiceDomain brewingAmountDice1 = new DiceDomain(4, 12, 200);
+                DiceDomain brewingAmountDice2 = new DiceDomain(4, 15, 200);
+                DiceDomain brewingAmountDice3 = new DiceDomain(4, 15, 250);
+                DiceDomain brewingAmountDice4 = new DiceDomain(4, 20, 200);
+                DiceDomain brewingAmountDice5 = new DiceDomain(4, 20, 250);
+                DiceDomain brewingAmountDice6 = new DiceDomain(4, 23, 250);
+
+                BrewingWaterAmountDice.add(brewingAmountDice1);
+                BrewingWaterAmountDice.add(brewingAmountDice2);
+                BrewingWaterAmountDice.add(brewingAmountDice3);
+                BrewingWaterAmountDice.add(brewingAmountDice4);
+                BrewingWaterAmountDice.add(brewingAmountDice5);
+                BrewingWaterAmountDice.add(brewingAmountDice6);
+
+                return BrewingWaterAmountDice;
+
+            }
+
+
+
+    
 }
