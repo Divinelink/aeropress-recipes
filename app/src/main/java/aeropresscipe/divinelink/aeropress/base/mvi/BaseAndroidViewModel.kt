ea44c90e4d385524eb0b5c20de22983e148b0016
Haris Andreolas <andreo.haris@gmail.com>
2022-06-15 01:29:53 +0300

    feat: base mvi setup WIP
    add BaseAndroidViewModel subclass

---
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/base/mvi/BaseAndroidViewModel.kt
@@ -0,0 +1,15 @@
+package aeropresscipe.divinelink.aeropress.base.mvi
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import java.lang.ref.WeakReference
+
+abstract class BaseAndroidViewModel<T : Any>(application: Application) :
+    AndroidViewModel(application),
+    MVIBaseView {
+    protected abstract var delegate: WeakReference<T>?
+
+    override fun onDestroy() {
+        delegate = null
+    }
+}
