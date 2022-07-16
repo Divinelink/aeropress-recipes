ba5e6067afd4c8813d28923fb707009dd00fba72
Haris Andreolas <andreo.haris@gmail.com>
2022-07-16 12:10:48 +0300

    feat: add numbers object to avoid magic numbers

--- core-util/src/main/java/gr/divinelink/core/util/extensions/StringExtensions.kt
+++ core-util/src/main/java/gr/divinelink/core/util/extensions/StringExtensions.kt
@@ -3,6 +3,7 @@ package gr.divinelink.core.util.extensions
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import gr.divinelink.core.util.constants.Numbers
 
 // Extension method that returns a Spanned text with Html elements.
 fun CharSequence.toSpanned(): Spanned {
@@ -12,4 +13,8 @@ fun CharSequence.toSpanned(): Spanned {
         @Suppress("DEPRECATION")
         Html.fromHtml(this.toString())
     }
+}
+
+fun Int.toFahrenheit(): Int {
+    return (this * (Numbers.NINE / Numbers.FIVE + Numbers.THIRTY_TWO))
 }
