3b78c63906756b996d747ef1917ed16b0889bca8
Haris Andreolas <andreo.haris@gmail.com>
2022-07-17 13:48:35 +0300

    feat: add time left properties on dice domain

--- app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/models/DiceDomain.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/generaterecipe/models/DiceDomain.kt
@@ -7,7 +7,9 @@ import java.io.Serializable
 @Entity(tableName = "Recipe")
 data class DiceDomain(
     var recipe: Recipe,
-    var isBrewing: Boolean = false
+    var isBrewing: Boolean = false,
+    var bloomTimeLeft: Long = 0L,
+    var brewTimeLeft: Long = 0L
 ) : Serializable {
     companion object {
         private const val serialVersionUID: Long = 1L
