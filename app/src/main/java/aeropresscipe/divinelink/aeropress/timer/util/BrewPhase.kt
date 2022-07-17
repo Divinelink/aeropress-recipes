5f1c74d8c5cfd6f401e47adfb0737482032ae353
Haris Andreolas <andreo.haris@gmail.com>
2022-07-17 13:49:57 +0300

    feat: add phase property on BrewState sealed class

--- app/src/main/java/aeropresscipe/divinelink/aeropress/timer/util/BrewPhase.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/timer/util/BrewPhase.kt
@@ -33,9 +33,35 @@ sealed class BrewState(
     @StringRes open val description: Int,
     open val brewWater: Int,
     open val brewTime: Long,
+    open val phase: Phase
 ) {
-    data class Bloom(val water: Int, val time: Long) : BrewState(title = R.string.bloomPhase, description = R.string.bloomPhaseWaterText, water, time)
-    data class Brew(val water: Int, val time: Long) : BrewState(title = R.string.brewPhase, description = R.string.brewPhaseNoBloom, water, time)
-    data class BrewWithBloom(val water: Int, val time: Long) : BrewState(title = R.string.brewPhase, description = R.string.brewPhaseWithBloom, water, time)
-    object Finished : BrewState(title = 0, description = 0, brewWater = 0, brewTime = 0)
+    data class Bloom(val water: Int, val time: Long) : BrewState(
+        title = R.string.bloomPhase,
+        description = R.string.bloomPhaseWaterText,
+        water,
+        time,
+        Phase.Bloom)
+
+    data class Brew(val water: Int, val time: Long) : BrewState(
+        title = R.string.brewPhase,
+        description = R.string.brewPhaseNoBloom,
+        water, time,
+        Phase.Brew
+    )
+
+    data class BrewWithBloom(val water: Int, val time: Long) : BrewState(
+        title = R.string.brewPhase,
+        description = R.string.brewPhaseWithBloom,
+        water,
+        time,
+        Phase.Brew
+    )
+
+    object Finished : BrewState(title = 0, description = 0, brewWater = 0, brewTime = 0, Phase.Finish)
+}
+
+enum class Phase {
+    Bloom,
+    Brew,
+    Finish
 }
