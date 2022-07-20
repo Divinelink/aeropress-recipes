fa8b049d88c689f89698f6f46982d74b6d0de955
Haris Andreolas <andreo.haris@gmail.com>
2022-07-20 19:28:30 +0300

    refactor: remove mvp files

--- app/src/main/java/aeropresscipe/divinelink/aeropress/history/ISharedPrefHistoryManager.java
+++
@@ -1,7 +0,0 @@
-package aeropresscipe.divinelink.aeropress.history;
-
-import android.content.Context;
-
-public interface ISharedPrefHistoryManager {
-    void setIsHistoryEmptyBool(Context ctx, boolean bool);
-}
