ea8a4b342552f680dd7168e0df7af145e8188d4f
Haris Andreolas <andreo.haris@gmail.com>
2022-08-05 13:22:39 +0300

    refactor: recipe card view xml
    replace layouts with compat drawables

--- app/src/main/java/aeropresscipe/divinelink/aeropress/customviews/RecipeCardView.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/customviews/RecipeCardView.kt
@@ -6,25 +6,17 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.FrameLayout
+import androidx.cardview.widget.CardView
 import gr.divinelink.core.util.extensions.toFahrenheit
 import java.util.Locale
 
-class RecipeCardView : FrameLayout {
+class RecipeCardView : CardView {
     var binding: ViewRecipeCardBinding = ViewRecipeCardBinding.inflate(LayoutInflater.from(context), this, false)
 
     constructor(context: Context) : this(context, null)
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         addView(binding.root)
-    }
-
-    fun enableLikeButton(enable: Boolean) {
-        if (enable) {
-            binding.likeRecipeLayout.visibility = View.VISIBLE
-        } else {
-            binding.likeRecipeLayout.visibility = View.GONE
-        }
     }
 
     fun setRecipe(card: RecipeCard) {
@@ -58,10 +50,14 @@ class RecipeCardView : FrameLayout {
         binding.likeRecipeLayout.visibility = card.likeButtonVisibility
     }
 
-    fun setOnLikeButtonListener(l: () -> Unit) {
+    /**
+     * Like button's visibility changes when you set a click listener.
+     */
+
+    fun setOnLikeButtonListener(action: () -> Unit) {
         binding.likeButton.visibility = View.VISIBLE
         binding.likeButton.setOnClickListener {
-            l.invoke()
+            action.invoke()
         }
     }
 
