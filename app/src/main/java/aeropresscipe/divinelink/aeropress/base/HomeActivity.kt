8bcce17b115f77655ca85c31b03b76f063ec64f8
Haris Andreolas <andreo.haris@gmail.com>
2022-08-31 09:17:56 +0300

    feat: avoid recreating fragments by fixing transaction

--- app/src/main/java/aeropresscipe/divinelink/aeropress/base/HomeActivity.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/base/HomeActivity.kt
@@ -1,6 +1,5 @@
 package aeropresscipe.divinelink.aeropress.base
 
-import gr.divinelink.core.util.utils.WindowUtil.setNavigationBarColor
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -12,89 +11,102 @@ import aeropresscipe.divinelink.aeropress.savedrecipes.SavedRecipesFragment
 import aeropresscipe.divinelink.aeropress.history.HistoryFragment
 import gr.divinelink.core.util.viewBinding.activity.viewBinding
 import android.view.MenuItem
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationBarView
-import com.google.android.material.navigation.NavigationBarView.OnItemReselectedListener
+import gr.divinelink.core.util.utils.setNavigationBarColor
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
     private val binding: ActivityHomeBinding by viewBinding()
 
+    private val fragments: Array<out Fragment> get() = arrayOf(recipeFragment, favoritesFragment, historyFragment)
+    private var selectedIndex = 0
+
+    private lateinit var recipeFragment: GenerateRecipeFragment
+    private lateinit var favoritesFragment: SavedRecipesFragment
+    private lateinit var historyFragment: HistoryFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setNavigationBarColor(this, ContextCompat.getColor(this, R.color.colorSurface2))
+        setNavigationBarColor(ContextCompat.getColor(this, R.color.colorSurface2))
 
         binding.bottomNavigation.setOnItemSelectedListener(onItemSelectedListener)
-        binding.bottomNavigation.setOnItemReselectedListener(onItemReselectedListener)
+//        binding.bottomNavigation.setOnItemReselectedListener(onItemReselectedListener)
 
-        supportFragmentManager
-            .beginTransaction()
-            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            .replace(R.id.fragment, GenerateRecipeFragment.newInstance())
-            .commit()
+        if (savedInstanceState == null) {
+            recipeFragment = GenerateRecipeFragment.newInstance()
+            favoritesFragment = SavedRecipesFragment.newInstance()
+            historyFragment = HistoryFragment.newInstance()
 
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment, recipeFragment, RECIPE_TAG)
+                .add(R.id.fragment, favoritesFragment, FAVORITES_TAG)
+                .add(R.id.fragment, historyFragment, HISTORY_TAG)
+                .commitNow()
+        } else {
+            selectedIndex = savedInstanceState.getInt(SELECTED_INDEX, 0)
+
+            recipeFragment = supportFragmentManager.findFragmentByTag(RECIPE_TAG) as GenerateRecipeFragment
+            favoritesFragment = supportFragmentManager.findFragmentByTag(FAVORITES_TAG) as SavedRecipesFragment
+            historyFragment = supportFragmentManager.findFragmentByTag(HISTORY_TAG) as HistoryFragment
+        }
+
+        val selectedFragment = fragments[selectedIndex]
+
+        selectFragment(selectedFragment)
     }
 
     override fun onResume() {
         super.onResume()
-//         When leaving from Timer Activity, set bottomNavigation to be the recipe button
-//         and restart the fragment, so we can see the resume button flashing.
+//       When leaving from Timer Activity, set bottomNavigation to be the recipe button
+//       and restart the fragment, so we can see the resume button flashing.
         binding.bottomNavigation.selectedItemId = R.id.recipe
-        supportFragmentManager
-            .beginTransaction()
-            .setTransition(FragmentTransaction.TRANSIT_NONE)
-            .replace(R.id.fragment, GenerateRecipeFragment.newInstance())
-            .commit()
-    }
-
-    private fun addGenerateRecipeFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment, GenerateRecipeFragment.newInstance())
-            .addToBackStack(null)
-            .commit()
-    }
-
-    private fun addSavedRecipesFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment, SavedRecipesFragment.newInstance())
-            .addToBackStack(null)
-            .commit()
-    }
-
-    private fun addHistoryFragment() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment, HistoryFragment.newInstance())
-            .addToBackStack(null)
-            .commit()
+        selectFragment(recipeFragment)
     }
 
     private var onItemSelectedListener = NavigationBarView.OnItemSelectedListener { item: MenuItem ->
         when (item.itemId) {
             R.id.recipe -> {
-                addGenerateRecipeFragment()
+                selectFragment(recipeFragment)
                 return@OnItemSelectedListener true
             }
             R.id.favorites -> {
-                addSavedRecipesFragment()
+                selectFragment(favoritesFragment)
                 return@OnItemSelectedListener true
             }
             R.id.history -> {
-                addHistoryFragment()
+                selectFragment(historyFragment)
                 return@OnItemSelectedListener true
             }
         }
         false
     }
+    /* todo add callback that scrolls to top
     var onItemReselectedListener = OnItemReselectedListener { item: MenuItem ->
         when (item.itemId) {
             R.id.recipe, R.id.favorites, R.id.history -> {
                 // Intentionally Blank.
             }
         }
+    }
+     */
+
+    override fun onSaveInstanceState(bundle: Bundle) {
+        super.onSaveInstanceState(bundle)
+        bundle.putInt(SELECTED_INDEX, selectedIndex)
+    }
+
+    private fun selectFragment(selectedFragment: Fragment) {
+        var transaction = supportFragmentManager.beginTransaction()
+        fragments.forEachIndexed { index, fragment ->
+            if (selectedFragment == fragment) {
+                transaction = transaction.attach(fragment)
+                selectedIndex = index
+            } else {
+                transaction = transaction.detach(fragment)
+            }
+        }
+        transaction.commit()
     }
 
     override fun onBackPressed() {
@@ -104,5 +116,12 @@ class HomeActivity : AppCompatActivity() {
         } else {
             binding.bottomNavigation.selectedItemId = R.id.recipe
         }
+    }
+
+    companion object {
+        private const val RECIPE_TAG = "RECIPE"
+        private const val FAVORITES_TAG = "FAVORITES"
+        private const val HISTORY_TAG = "HISTORY"
+        private const val SELECTED_INDEX = "SELECTED_INDEX"
     }
 }
