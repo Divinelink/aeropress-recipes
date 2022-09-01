3865c66d539b5f0504a22f49edecd622d5644774
Haris Andreolas <andreo.haris@gmail.com>
2022-09-01 11:46:51 +0300

    feat: add updatePaddingAnimator extension

--- core-util/src/main/java/gr/divinelink/core/util/extensions/ViewExtensions.kt
+++ core-util/src/main/java/gr/divinelink/core/util/extensions/ViewExtensions.kt
@@ -2,9 +2,9 @@ package gr.divinelink.core.util.extensions
 
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.view.View
 import android.widget.TextView
-
 
 fun View?.fadeOut() {
     this?.animate()
@@ -17,6 +17,35 @@ fun View?.fadeOut() {
             }
         })
 }
+
+fun View?.updatePaddingAnimator(
+    start: Int? = null,
+    end: Int? = null,
+    top: Int? = null,
+    bottom: Int? = null
+) {
+    if (this == null) return
+    start?.let { ValueAnimator.ofInt(this.paddingStart, it) }
+        ?.also { it.addUpdateListener { valueAnimator -> this.padding(left = valueAnimator.animatedValue as Int) } }
+        ?.also { it.duration = 200 }
+        ?.run { start() }
+
+    end?.let { ValueAnimator.ofInt(this.paddingEnd, it) }
+        ?.also { it.addUpdateListener { valueAnimator -> this.padding(right = valueAnimator.animatedValue as Int) } }
+        ?.also { it.duration = 200 }
+        ?.run { start() }
+
+    top?.let { ValueAnimator.ofInt(this.paddingStart, it) }
+        ?.also { it.addUpdateListener { valueAnimator -> this.padding(top = valueAnimator.animatedValue as Int) } }
+        ?.also { it.duration = 200 }
+        ?.run { start() }
+
+    bottom?.let { ValueAnimator.ofInt(this.paddingBottom, it) }
+        ?.also { it.addUpdateListener { valueAnimator -> this.padding(bottom = valueAnimator.animatedValue as Int) } }
+        ?.also { it.duration = 200 }
+        ?.run { start() }
+}
+
 
 infix fun TextView?.updateTextWithFade(text: CharSequence) {
     this?.animate()
