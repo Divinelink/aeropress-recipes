4fc71b87df9e22118f23111722535d955b3927be
Haris Andreolas <andreo.haris@gmail.com>
2022-10-23 14:03:12 +0300

    feat: add useCases to receive theme from datastore

---
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/settings/app/appearance/use_case/ObserveThemeModeUseCase.kt
@@ -0,0 +1,24 @@
+package aeropresscipe.divinelink.aeropress.settings.app.appearance.use_case
+
+import aeropresscipe.divinelink.aeropress.base.di.DefaultDispatcher
+import aeropresscipe.divinelink.aeropress.base.di.PreferenceStorage
+import aeropresscipe.divinelink.aeropress.base.keyvalue.Theme
+import aeropresscipe.divinelink.aeropress.base.keyvalue.themeFromStorageKey
+import gr.divinelink.core.util.domain.FlowUseCase
+import gr.divinelink.core.util.domain.Result
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+open class ObserveThemeModeUseCase @Inject constructor(
+    private val preferenceStorage: PreferenceStorage,
+    @DefaultDispatcher dispatcher: CoroutineDispatcher
+) : FlowUseCase<Unit, Theme>(dispatcher) {
+    override fun execute(parameters: Unit): Flow<Result<Theme>> {
+        return preferenceStorage.selectedTheme.map {
+            val theme = themeFromStorageKey(it) ?: Theme.SYSTEM
+            Result.Success(theme)
+        }
+    }
+}
