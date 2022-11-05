2cbeea0ff4182c0c4d899e6e554cfde222da3fab
Haris Andreolas <andreo.haris@gmail.com>
2022-11-05 13:23:40 +0200

    feat: add beans tracker fragment and its viewModel

---
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/beans/BeansTrackerFragment.kt
@@ -0,0 +1,37 @@
+package aeropresscipe.divinelink.aeropress.beans
+
+import aeropresscipe.divinelink.aeropress.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BeansTrackerFragment : Fragment() {
+
+    private lateinit var composeView: ComposeView
+    private val viewModel: BeansTrackerViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return ComposeView(requireContext()).also {
+            composeView = it
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        composeView.setContent {
+
+            SelectOptionView(onClick = { /*TODO*/ },
+                hint = R.string.Beans__process
+            )
+//            BeansScreen(
+//
+//            )
+        }
+    }
+}
