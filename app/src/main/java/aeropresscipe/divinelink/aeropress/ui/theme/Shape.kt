ee7002fbeacaf01961f0fade73a98fbd32faa4b2
Haris Andreolas <andreo.haris@gmail.com>
2022-11-06 11:35:54 +0200

    feat: overriding MD2 theme for color picker

---
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/ui/theme/Shape.kt
@@ -0,0 +1,6 @@
+package aeropresscipe.divinelink.aeropress.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val ButtonShape = RoundedCornerShape(8.dp)
