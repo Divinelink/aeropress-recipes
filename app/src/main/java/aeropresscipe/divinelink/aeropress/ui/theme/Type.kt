3493f4e48ac57bbb1c520b41221000e8be686c98
Haris Andreolas <andreo.haris@gmail.com>
2022-12-02 12:07:22 +0200

    fix: app font family

--- app/src/main/java/aeropresscipe/divinelink/aeropress/ui/theme/Type.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/ui/theme/Type.kt
@@ -5,27 +5,21 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 private val appFontFamily = FontFamily(
-    fonts = listOf(
-        Font(
-            resId = R.font.font_light,
-//            weight = FontWeight.W300, FontWeight.Light
-            style = FontStyle.Normal
-        ),
-        Font(
-            resId = R.font.font_regular,
-//            weight = FontWeight.W400, FontWeight.Normal
-            style = FontStyle.Normal
-        ),
-        Font(
-            resId = R.font.font_medium,
-//            weight = FontWeight.W500, FontWeight.Medium
-            style = FontStyle.Normal
-        )
+    Font(
+        resId = R.font.font_light,
+        weight = FontWeight.W300, // FontWeight.Light
+    ),
+    Font(
+        resId = R.font.font_regular,
+        weight = FontWeight.W400, // FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.font_medium,
+        weight = FontWeight.W500, // FontWeight.Medium
     )
 )
 
