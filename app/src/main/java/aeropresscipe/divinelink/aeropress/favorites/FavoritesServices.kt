1d363b10a2ea6a224490eba16239345976da03df
Haris Andreolas <andreo.haris@gmail.com>
2022-12-09 01:41:20 +0200

    refactor: remove old files

--- app/src/main/java/aeropresscipe/divinelink/aeropress/favorites/FavoritesServices.kt
+++ app/src/main/java/aeropresscipe/divinelink/aeropress/favorites/FavoritesServices.kt
@@ -1,37 +1,32 @@
 package aeropresscipe.divinelink.aeropress.favorites
 
-import aeropresscipe.divinelink.aeropress.base.di.IoDispatcher
-import aeropresscipe.divinelink.aeropress.history.HistoryDao
 import aeropresscipe.divinelink.aeropress.recipe.models.Recipe
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.withContext
-import javax.inject.Inject
 
 interface IFavoritesServices {
-    suspend fun getFavorites(): List<Favorites>
+    suspend fun fetchAllFavorites(): List<Favorites>
     suspend fun deleteRecipe(recipe: Recipe): List<Favorites>
 }
 
-class FavoritesServices @Inject constructor(
-    private val historyDao: HistoryDao,
-    private val favoritesDao: FavoritesDao,
-    @IoDispatcher private val dispatcher: CoroutineDispatcher
-) : IFavoritesServices {
-    override suspend fun getFavorites(): List<Favorites> {
-        return withContext(dispatcher) {
-            favoritesDao.favorites
-        }
-    }
-
-    override suspend fun deleteRecipe(recipe: Recipe): List<Favorites> {
-        return withContext(dispatcher) {
-            val recipes = favoritesDao
-            val history = historyDao
-
-            recipes.delete(recipe)
-            history.updateLike(recipe = recipe, false)
-
-            return@withContext recipes.favorites
-        }
-    }
-}
+// class FavoritesServices @Inject constructor(
+//    private val historyDao: HistoryDao,
+//    private val favoritesDao: FavoritesDao,
+//    @IoDispatcher private val dispatcher: CoroutineDispatcher
+// ) : IFavoritesServices {
+//    override suspend fun fetchAllFavorites(): List<Favorites> {
+//        return withContext(dispatcher) {
+//            favoritesDao.favorites
+//        }
+//    }
+//
+//    override suspend fun deleteRecipe(recipe: Recipe): List<Favorites> {
+//        return withContext(dispatcher) {
+//            val recipes = favoritesDao
+//            val history = historyDao
+//
+//            recipes.delete(recipe)
+//            history.updateLike(recipe = recipe, false)
+//
+//            return@withContext recipes.favorites
+//        }
+//    }
+// }
